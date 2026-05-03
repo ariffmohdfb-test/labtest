d91f5eb593459f3d89bd6e54bb3c598a741aa4b0
ariffmohdfb-test <ariffmohdfb@gmail.com>
2026-05-03 13:37:11 +0000

    Lab Test

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ let globalFeed = [
     { id: 1, username: "System", message: "Welcome to MILA University!" }
 ];
 
+app.get('/api/feed', (req, res) => {
+    res.json(globalFeed);
+});
+
+app.post('/api/feed', (req, res) => {
+    const { username, message } = req.body;
+
+    if (!username || !message) {
+        return res.status(400).json({ error: "Required: Username and Message" });
+    }
+
+    globalFeed.push({
+        id: Date.now(),
+        username: username,
+        message: message
+    });
+
+    res.status(201).json({ message: "Message Created Successfully" });
+});
+
 app.listen(PORT, () => {
     console.log(`API Server is running on port ${PORT}`);
 });
